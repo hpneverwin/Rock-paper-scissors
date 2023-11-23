eddb9aeab3ee1b1b93c7e1c2cf9a488055223ab3
hpneverwin <luisenglish0203@gmail.com>
2023-11-23 22:27:39 +0700

    add getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function getComputerChoice(){
+    const options = ["rock","paper","scissor"];
+    let choice = options[Math.floor(Math.random()*3)];
+}
+
+function playRound(playerSelection,computerSelection){
+    
+}
